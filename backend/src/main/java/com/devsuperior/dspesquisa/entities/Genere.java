717a4507c4053eb8376020334b104aec7f7f9c4f
Jarvis <superpujol@gmail.com>
2020-09-15 17:41:40 -0300

    adição entities e repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genere.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dspesquisa.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_genre")
+public class Genere implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private  String name;
+
+    @OneToMany(mappedBy = "genre")
+    private List<Game> gameList = new ArrayList<>();
+
+    public Genere() {
+    }
+
+    public Genere(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Game> getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(List<Game> gameList) {
+        this.gameList = gameList;
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GenreRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import com.devsuperior.dspesquisa.entities.Genere;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<Genere, Long> {
+}
